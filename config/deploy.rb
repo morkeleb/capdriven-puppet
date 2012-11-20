25dfe2e82d77666f5be6a3c24c699d7391b6054f
Morten Nielsen <morten@morkeleb.com>
2012-11-20 22:58:08 +0100

    adding a journal describing growth of repo, adding ssh keys and server to setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,15 @@ set :repository,  "git@github.com:morkeleb/capdriven-puppet.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "ec2-176-34-161-163.eu-west-1.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+
+
+ssh_options[:keys] = ["#{ENV['HOME']}/.ec2/capdriven-puppet.pem"] #unless 'development' == rails_env
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
---
+++ journal.md
@@ -0,0 +1,11 @@
+Start with an empty repo
+
+Capify it
+
+add git configuration
+
+add server to bootstrap, after creating it on ec2. I'm using Amazon Linux.
+Starting with one server.
+
+add shh config for connecting to ec2
+
