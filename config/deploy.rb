580ba4354aea5f556953149d839d1b24464a3698
Morten Nielsen <morten@morkeleb.com>
2012-11-20 23:09:36 +0100

    adding installation of puppet using yum

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,9 +9,35 @@ role :web, "ec2-176-34-161-163.eu-west-1.compute.amazonaws.com"                 
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-
+   default_run_options[:pty] = true # need to tell ssh to do this so we can use try_sudo
 
 ssh_options[:keys] = ["#{ENV['HOME']}/.ec2/capdriven-puppet.pem"] #unless 'development' == rails_env
+
+#bootstrap from https://gist.github.com/3072375
+#modified to install puppet on the server
+namespace :bootstrap do
+  task :default do
+    # Specific RVM string for managing Puppet; may or may not match the RVM string for the application
+    set :user, "ec2-user"
+
+    # Set the default_shell to "bash" so that we don't use the RVM shell which isn't installed yet...
+    set :default_shell, "bash"
+
+    try_sudo("yum -y install puppet") #install puppet, -y assumes answer yes on all questions.
+
+    # We tar up the puppet directory from the current directory -- the puppet directory within the source code repository
+    system("tar cczf 'puppet.tgz' puppet/")
+    upload("puppet.tgz","/home/#{user}",:via => :scp)
+
+    # Untar the puppet directory, and place at /etc/puppet -- the default location for manifests/modules
+    run("tar xzf puppet.tgz")
+    try_sudo("rm -rf /etc/puppet")
+    try_sudo("mv /home/#{user}/puppet/ /etc/puppet")
+
+    # Bootstrap RVM/Puppet!
+    try_sudo("bash /etc/puppet/bootstrap.sh")
+  end 
+end
 
 
 # if you want to clean up old releases on each deploy uncomment this:
--- journal.md
+++ journal.md
@@ -9,3 +9,6 @@ Starting with one server.
 
 add shh config for connecting to ec2
 
+add bootstrap task and make sure yum installs puppet when bootstraping.
+yum install is idempotent so we can rerun the bootstrap.
+I think this is all we need and we can do the type specific bootstrapping.
